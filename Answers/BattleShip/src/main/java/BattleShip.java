d518b0dd838a3b17daa44210e6dfb312e32e30eb
Yasin-Taheri <yyyasintaheri1385@gmail.com>
2025-03-06 23:44:03 +0330

    initializeGrid completed. canPlaceShip completed. placeShips completed. isValidInput completed. playerTurn completed. allShipsSunk completed. isGameOver completed. printGrid completed.

--- Answers/BattleShip/src/main/java/BattleShip.java
+++ Answers/BattleShip/src/main/java/BattleShip.java
@@ -213,7 +213,7 @@ public class BattleShip {
         }
         String colStr = input.substring(1);
         for(int i = 0; i < colStr.length(); i++) {
-            if(Character.isDigit(colStr.charAt(i))) {
+            if(!Character.isDigit(colStr.charAt(i))) {
                 return false;
             }
         }
@@ -231,6 +231,12 @@ public class BattleShip {
       @param grid The tracking grid to print.
      */
     static void printGrid(char[][] grid) {
-        //todo
+        System.out.println("  1 2 3 4 5 6 7 8 9 10");
+        for(int i = 0; i < GRID_SIZE; i++) {
+        System.out.print((char)('A' + i) + " ");
+        for(int j = 0; j < GRID_SIZE; j++) {
+            System.out.print(grid[i][j] + " ");
+        }
+        System.out.println();}
     }
 }
